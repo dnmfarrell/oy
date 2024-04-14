3654ba489c4bc6c23c1dbc27691917cc9edbb0a4
David Farrell <1469333+dnmfarrell@users.noreply.github.com>
2024-04-14 14:26:58 -0400

    feat: edit task descriptions

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 oy
 ==
-A POSIX-shell compatible task manager productivity app. Uses `sqlite3`.
+A stupid-simple POSIX-shell compatible todo list app. Uses `sqlite3`.
 
     oy [Option]
     
     Options
-      a|add  <desc>  add a task
-      d|done <#>     mark task as done
-      h|help         print this help
+      a|add  <desc>       add a task
+      d|done <#>          mark task as done
+      e|edit <#> <desc>   edit task description
+      h|help              print this help
 
--- new.sql
+++ new.sql
@@ -1,13 +1,12 @@
-drop table if exists tag;
-create table tag (
+drop table if exists "set";
+create table "set" (
   key integer primary key autoincrement,
-  grp varchar(255) not null,
-  txt varchar(255) not null,
-  unique (grp, txt)
+  name varchar(255) not null,
+  unique (name)
 );
-insert into tag (grp, txt) values
-('status', 'new'),
-('status', 'done');
+insert into "set" (name) values
+('version'),
+('status');
 
 drop table if exists task;
 create table task (
@@ -17,8 +16,62 @@ create table task (
 
 drop table if exists task_tag;
 create table task_tag (
-  task_key integer not null,
-  tag_key integer not null,
+  key integer primary key autoincrement,
+  txt varchar(255) not null,
   t timestamp default (datetime('now')),
-  primary key (task_key, tag_key)
+  cargo varchar(255),
+  task_key integer not null references task (key),
+  set_key integer not null references "set" (key)
 );
+
+drop view if exists vw_task_tags;
+create view vw_task_tags as
+select  tt.task_key,
+        tt.t,
+        datetime(tt.t, 'localtime') lt,
+        tt.set_key,
+        "set".name set_name,
+        tt.txt tag_txt
+from task_tag tt
+join "set" on "set".key = tt.set_key
+left join task_tag st
+on st.task_key = tt.task_key
+  and st.set_key = tt.set_key
+  and st.key > tt.key
+where st.task_key is null
+order by tt.task_key, tt.set_key
+;
+
+drop view if exists vw_open_tasks;
+create view vw_open_tasks as
+select  task_key,
+        min(t) created,
+        max(t) updated,
+        min(lt) created_l,
+        max(lt) updated_l,
+        group_concat(set_name||':'||tag_txt) tags
+from vw_task_tags
+group by 1
+having sum(case when set_name = 'status' and tag_txt = 'open' then 1 else 0 end) = 1
+;
+
+drop view if exists vw_done_tasks;
+create view vw_done_tasks as
+select  task_key,
+        min(t) created,
+        max(t) updated,
+        min(lt) created_l,
+        max(lt) updated_l,
+        group_concat(set_name||':'||tag_txt) tags
+from vw_task_tags
+group by 1
+having sum(case when set_name = 'status' and tag_txt = 'done' then 1 else 0 end) = 1
+;
+
+drop view if exists vw_next_version;
+create view vw_next_version as
+select tag_txt+1, task.txt, task_key, set_key
+from vw_task_tags
+join task on key = task_key
+where set_key = 1
+;
